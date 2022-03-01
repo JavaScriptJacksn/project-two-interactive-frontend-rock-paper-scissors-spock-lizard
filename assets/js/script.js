2b6fafa7639f406ef51390a1bed5bf4e8e5de044
JavaScriptJacksn <jbjackson247@gmail.com>
2022-03-01 17:54:34 +0000

    Added modal pop-up boxes for both player 1 and player 2, the method I used wasn't very effective as it repeated lines of code, where it would be better to use a data type for each button to take into the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,39 @@ function rulesClose(){
     modal.classList.remove("popup-active");
     overlay.classList.remove("overlay-active");
 }
+
+/*Functions for opening and closing player 1 change move modal box*/
+
+function player1ChangeMove (){
+    console.log("Player 1 select a move");
+    let modal = document.getElementById("player1-move-box");
+    let overlay = document.getElementById("overlay");
+    modal.classList.add("popup-active");
+    overlay.classList.add("overlay-active");
+}
+
+function player1MoveClose(){
+    console.log("Player1MoveClose called")
+    let modal = document.getElementById("player1-move-box");
+    let overlay = document.getElementById("overlay");
+    modal.classList.remove("popup-active");
+    overlay.classList.remove("overlay-active");
+}
+
+/*Functions for opening and closing player 2 change move modal box*/
+
+function player2ChangeMove (){
+    console.log("Player 1 select a move");
+    let modal = document.getElementById("player2-move-box");
+    let overlay = document.getElementById("overlay");
+    modal.classList.add("popup-active");
+    overlay.classList.add("overlay-active");
+}
+
+function player2MoveClose(){
+    console.log("Player1MoveClose called")
+    let modal = document.getElementById("player2-move-box");
+    let overlay = document.getElementById("overlay");
+    modal.classList.remove("popup-active");
+    overlay.classList.remove("overlay-active");
+}
