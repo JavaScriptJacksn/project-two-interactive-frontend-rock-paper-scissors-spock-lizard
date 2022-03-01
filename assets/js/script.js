4e14430a1fdd646fbe56d0b57a159330020313f0
JavaScriptJacksn <jbjackson247@gmail.com>
2022-03-01 18:50:41 +0000

    Changed the functions to take in a parameter of the id of the modal to present, cutting down the previous 6 functions used to 2, for open and close only for better readability and robust code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,19 @@
 /*Functions for opening and closing the rules modal box*/
 
-function popUp (){
+function popUp (modal){
     console.log("popUp called");
-    let modal = document.getElementById("rules");
+    console.log(modal);
+    let box = document.getElementById(modal);
     let overlay = document.getElementById("overlay");
-    modal.classList.add("popup-active");
+    box.classList.add("popup-active");
     overlay.classList.add("overlay-active");
 }
 
-function rulesClose(){
-    console.log("rulesClose called")
-    let modal = document.getElementById("rules");
+function popupClose(modal){
+    console.log("popUp called");
+    console.log(modal);
+    let box = document.getElementById(modal);
     let overlay = document.getElementById("overlay");
-    modal.classList.remove("popup-active");
-    overlay.classList.remove("overlay-active");
-}
-
-/*Functions for opening and closing player 1 change move modal box*/
-
-function player1ChangeMove (){
-    console.log("Player 1 select a move");
-    let modal = document.getElementById("player1-move-box");
-    let overlay = document.getElementById("overlay");
-    modal.classList.add("popup-active");
-    overlay.classList.add("overlay-active");
-}
-
-function player1MoveClose(){
-    console.log("Player1MoveClose called")
-    let modal = document.getElementById("player1-move-box");
-    let overlay = document.getElementById("overlay");
-    modal.classList.remove("popup-active");
-    overlay.classList.remove("overlay-active");
-}
-
-/*Functions for opening and closing player 2 change move modal box*/
-
-function player2ChangeMove (){
-    console.log("Player 1 select a move");
-    let modal = document.getElementById("player2-move-box");
-    let overlay = document.getElementById("overlay");
-    modal.classList.add("popup-active");
-    overlay.classList.add("overlay-active");
-}
-
-function player2MoveClose(){
-    console.log("Player1MoveClose called")
-    let modal = document.getElementById("player2-move-box");
-    let overlay = document.getElementById("overlay");
-    modal.classList.remove("popup-active");
+    box.classList.remove("popup-active");
     overlay.classList.remove("overlay-active");
 }
